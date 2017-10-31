49fc51f13d8134461ea3a4942bf8399a18265c47
Rob Bronson <robert.j.bronson@gmail.com>
2017-10-31 16:13:32 -0700

    reworking step1

--- robert_pyramid_learning_journal/robert_pyramid_learning_journal/views/default.py
+++ robert_pyramid_learning_journal/robert_pyramid_learning_journal/views/default.py
@@ -10,23 +10,27 @@ HERE = os.path.dirname(__file__)
 
 def list_view(request):
     """Route user to list of all entries in LJ."""
-    imported_text = io.open(os.path.join(HERE, '../templates/homepage.html')).read()
-    return Response(imported_text)
+    path = os.path.join(HERE, '../templates/homepage.html')
+    with io.open(path) as imported_text:
+        return Response(imported_text.read())
 
 
 def detail_view(request):
     """Route user to detailed entry in LJ."""
-    imported_text = io.open(os.path.join(HERE, '../templates/detail-entry.html')).read()
-    return Response(imported_text)
+    path = os.path.join(HERE, '../templates/detail-entry.html')
+    with io.open(path) as imported_text:
+        return Response(imported_text.read())
 
 
 def create_view(request):
     """Route user to create entry page in LJ."""
-    imported_text = io.open(os.path.join(HERE, '../templates/new-entry.html')).read()
-    return Response(imported_text)
+    path = os.path.join(HERE, '../templates/new-entry.html')
+    with io.open(path) as imported_text:
+        return Response(imported_text.read())
 
 
 def update_view(request):
     """Route user to update entry page in LJ."""
-    imported_text = io.open(os.path.join(HERE, '..templates/edit-entry.html')).read()
-    return Response(imported_text)
+    path = os.path.join(HERE, '../templates/edit-entry.html')
+    with io.open(path) as imported_text:
+        return Response(imported_text.read())
