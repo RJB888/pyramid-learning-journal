33c3e6a0ab2e5578ce2129cc726f30567907ab5c
Rob Bronson <robert.j.bronson@gmail.com>
2017-11-03 17:22:19 -0700

    Restructured file structure

--- robert_pyramid_learning_journal/views/__init__.py
+++ robert_pyramid_learning_journal/views/__init__.py
@@ -1,7 +1,6 @@
-"""Writing view routes."""
+"""Writing views routes."""
 
-
-from robert_pyramid_learning_journal.views.default import(
+from robert_pyramid_learning_journal.views.default import (
     list_view,
     detail_view,
     create_view,
@@ -11,7 +10,7 @@ from robert_pyramid_learning_journal.views.default import(
 
 def includeme(config):
     """."""
-    config.add_view(list_view, route_name="list_view")
-    config.add_view(detail_view, route_name="detail_view")
-    config.add_view(create_view, route_name="create_view")
-    config.add_view(update_view, route_name="update_view")
+    config.add_view(list_view, route_name='list_view')
+    config.add_view(detail_view, route_name='detail_view')
+    config.add_view(create_view, route_name='create_view')
+    config.add_view(update_view, route_name='update_view')
--- robert_pyramid_learning_journal/views/default.py
+++ robert_pyramid_learning_journal/views/default.py
@@ -1,36 +1,77 @@
-"""Views for Pyramid Learning Journal."""
+"""Create callables for calling routes."""
+from pyramid.view import view_config
+from datetime import datetime
+from pyramid.httpexceptions import HTTPNotFound
+# from pyramid.response import Response
+# import os
+# import io
 
-from pyramid.response import Response
+# HERE = os.path.dirname(__file__)
 
-import io
-import os
+FMT = "%m/%d/%Y"
 
-HERE = os.path.dirname(__file__)
+POST = [
+    {'creation_date': datetime.strptime('11/1/2017', FMT),
+     'title': 'LJ1', 'author': 'robert Mother-Effin bronson',
+     'body': 'I am the baddest b!tch on the block.', 'id': 1},
+    {'creation_date': datetime.strptime('11/1/2017', FMT),
+     'title': 'LJ1', 'author': 'robert Mother-Effin bronson',
+     'body': 'I am the baddest b!tch on the block.', 'id': 2},
+    {'creation_date': datetime.strptime('11/1/2017', FMT),
+     'title': 'LJ1', 'author': 'robert Mother-Effin bronson',
+     'body': 'I am the baddest b!tch on the block.', 'id': 3},
+    {'creation_date': datetime.strptime('11/1/2017', FMT),
+     'title': 'LJ1', 'author': 'robert Mother-Effin bronson',
+     'body': 'I am the baddest b!tch on the block.', 'id': 4},
+    {'creation_date': datetime.strptime('11/1/2017', FMT),
+     'title': 'LJ1', 'author': 'robert Mother-Effin bronson',
+     'body': 'I am the baddest b!tch on the block.', 'id': 5}
+]
 
 
+@view_config(route_name='list_view',
+             renderer='robert_pyramid_learning_journal:templates/homepage.jinja2')
 def list_view(request):
-    """Route user to list of all entries in LJ."""
-    path = os.path.join(HERE, '../templates/homepage.html')
-    with io.open(path) as imported_text:
-        return Response(imported_text.read())
+    """Parse file path and pass it to response to serve home page."""
+    return {'ljposts': POST,
+            'title': 'robert LJ',
+            'image': "home-bg.jpg"}
 
 
+@view_config(route_name='detail_view',
+             renderer='robert_pyramid_learning_journal:templates/detail-entry.jinja2')
 def detail_view(request):
-    """Route user to detailed entry in LJ."""
-    path = os.path.join(HERE, '../templates/detail-entry.html')
-    with io.open(path) as imported_text:
-        return Response(imported_text.read())
+    """Parse file path and pass it to response to serve home page."""
+    post_id = int(request.matchdict['id'])
+    for post in POST:
+        if post['id'] == post_id:
+            return {'ljpost': post,
+                    'title': post['title'],
+                    'image': 'post-bg.jpg'}
+
+    raise HTTPNotFound
 
 
+@view_config(route_name='create_view',
+             renderer='robert_pyramid_learning_journal:templates/new-entry.jinja2')
 def create_view(request):
-    """Route user to create entry page in LJ."""
-    path = os.path.join(HERE, '../templates/new-entry.html')
-    with io.open(path) as imported_text:
-        return Response(imported_text.read())
+    """Parse file path and pass it to response to serve home page."""
+    # does anything go here??
+    return {'title': 'Create New Entry',
+            'image': 'new-entry.jpg'}
 
 
+@view_config(route_name='update_view',
+             renderer='robert_pyramid_learning_journal:templates/edit-entry.jinja2')
 def update_view(request):
-    """Route user to update entry page in LJ."""
-    path = os.path.join(HERE, '../templates/edit-entry.html')
-    with io.open(path) as imported_text:
-        return Response(imported_text.read())
+    """Parse file path and pass it to response to serve home page."""
+    # get post ID from detail_view page... import it?
+    # do same filter fn from detail_view
+    post_id = int(request.matchdict['id'])
+    for post in POST:
+        if post['id'] == post_id:
+            return {'ljpost': post['body'],
+                    'title': post['title'],
+                    'image': 'post-bg.jpg'}
+
+    raise HTTPNotFound
