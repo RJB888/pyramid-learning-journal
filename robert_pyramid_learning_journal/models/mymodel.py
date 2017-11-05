8ab4c0bc105cf98a936652568c4f409f15568c02
Rob Bronson <robert.j.bronson@gmail.com>
2017-11-05 15:09:21 -0800

    have it set up to pull data from file and create database.  Can't access db in psql.

--- robert_pyramid_learning_journal/models/mymodel.py
+++ robert_pyramid_learning_journal/models/mymodel.py
@@ -8,11 +8,11 @@ from sqlalchemy import (
 from .meta import Base
 
 
-class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
+# class MyModel(Base):
+#     __tablename__ = 'models'
+#     id = Column(Integer, primary_key=True)
+#     name = Column(Text)
+#     value = Column(Integer)
 
 
 class JournalEntry(Base):
@@ -20,6 +20,7 @@ class JournalEntry(Base):
     id = Column(Integer, primary_key=True)
     title = Column(Text)
     body = Column(Text)
-    date = Column()
+    author = Column(Text)
+    date = Column(Text)
 
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
+# Index('my_index', JournalEntry.title, unique=True, mysql_length=255)
