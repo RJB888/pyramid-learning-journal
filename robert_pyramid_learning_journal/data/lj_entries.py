7b3feb8ae31dc4646d00f41f2e000fbc33998602
Rob Bronson <robert.j.bronson@gmail.com>
2017-11-22 08:39:31 -0800

    added lj data to current

--- robert_pyramid_learning_journal/data/lj_entries.py
+++ robert_pyramid_learning_journal/data/lj_entries.py
@@ -1,6 +1,111 @@
 """My Learning Journals."""
 
 ENTRIES = [
+
+    {
+        "title": "Day 27",
+        "id": 27,
+        "date": "2017-11-22",
+        "body": "I'm getting so far behind that I feel almost hopeless about getting the work done.  I'll keep getting at it, and working the things, but in the end, I'm not sure I'll have a good grasp of any particular thing.  More like, just enough 'copy and paste' to get a decent grade.  Its hard to have the time to actually learn what the things are we are doing when we have to just spit out work as fast as possible just to keep up with the pace.  I'm not sure its a method thats conducive to learning....",
+        "author": "Rob Bronson"
+
+    },
+    {
+        "title": "Day 26",
+        "id": 26,
+        "date": "2017-11-21",
+        "body": "Well, Django.  I thought it seemed easy enough, but then there's all the stuff that we don't really know, and have to learn.  Since it seems to follow the same general idea of pyramid, I'm hoping it won't be too hard to wrap my head around.  We will see.  Oh, and I have to figure out how to get my Learning journal up to speed... :(",
+        "author": "Rob Bronson"
+
+    },
+    {
+        "title": "Day 25",
+        "id": 25,
+        "date": "2017-11-18",
+        "body": "Well today was presentations.. so its over.... oh wait, nope, just halfway there.  The presentation went ok.  Alexa of course screwed us on live demo... but thats what live demos are for.... so . whatever.  Feel bad that Nick never got his message....",
+        "author": "Rob Bronson"
+
+    },
+    {
+        "title": "Day 24",
+        "id": 24,
+        "date": "2017-11-17",
+        "body": "Today we worked on tests and started prepping for the presentation.  I also cleaned up the readme.  It may be the most useful readme I've been a part of.  The project is fine.... I wish we had been able to get the other functionality working, but it wasn't in the cards.",
+        "author": "Rob Bronson"
+
+    },
+    {
+        "title": "Day 23",
+        "id": 23,
+        "date": "2017-11-16",
+        "body": "Today we decided to ditch Amazon's built in feature for confirming input and prompting for information.  Then we proceeded  to spend 3 hours coding it ourselves, and I feel like we may not have done as good a job as Amazon did.  We have most of the basic functionality working, we just have to tune it so that Alexa doesn't get confused between confirming data and trying to launch a new intent.",
+        "author": "Rob Bronson"
+
+    },
+    {
+        "title": "Day 22",
+        "id": 22,
+        "date": "2017-11-15",
+        "body": "Some days it doesn't pay to get out of bed.  Today was an exercise in futility.  For being an amazingly huge tech company, Amazon is woefully lacking in documenting the Alexa skill process.",
+        "author": "Rob Bronson"
+
+    },
+    {
+        "title": "Day 21",
+        "id": 21,
+        "date": "2017-11-14",
+        "body": "I may be the only one still using this journal for daily journals.  My Pyramid's not quite ready yet to transfer this stuff over.  First day of project week went well, we got a lot done.  Hoping to be close to MVP tomorrow.",
+        "author": "Rob Bronson"
+
+    },
+    {
+        "title": "Day 20",
+        "id": 20,
+        "date": "2017-11-12",
+        "body": "Late again. but of course Im turning it in anyway.  I thought I wouldn't get much out of the career day,  but I did end up getting some good advice and work done on my resume... not complete by any means, but I feel its better suited to get me into the tech industry.",
+        "author": "Rob Bronson"
+
+    },
+    {
+        "title": "Day 19",
+        "id": 19,
+        "date": "2017-11-10",
+        "body": "I feel like I am getting a better grip on things.. Slightly slower pace this week.  I have been going back through the lecture vids and it is helping.",
+        "author": "Rob Bronson"
+
+    },
+    {
+        "title": "Day 18",
+        "id": 18,
+        "date": "2017-11-09",
+        "body": "Made some progress today with Pyramid Journal.  Have new post and update post working.  Just need to finish tests.  Then move on to the security stuff.",
+        "author": "Rob Bronson"
+
+    },
+    {
+        "title": "Day 17",
+        "id": 17,
+        "date": "2017-11-08",
+        "body": "Still adding entries here because I don't quite have my Python learning journal up and running.  So here u go.  Today, i got some help with the graphs and traversal, feeling better about that.  Still working on the pyramid stuff...i guess i better get a move on before we get into django and I really die.  Ttfn.",
+        "author": "Rob Bronson"
+
+    },
+    {
+        "title": "Day 16",
+        "id": 16,
+        "date": "2017-11-07",
+        "body": "Got some help with db queries.  Tried a code kata.. yeah, nope.  Need another re-work day.",
+        "author": "Rob Bronson"
+
+    },
+    {
+        "title": "Day 15",
+        "id": 15,
+        "date": "2017-11-04",
+        "body": "Bleached entry... because people read these things.   I didn't sit through the code review today, because its even more difficult for things to stay on track when Nick isn't there than when he is.  After the initial code review we went through 'my' code base for the Pyramid project, which was frustrating because even though it would completely crash on my system, when Nick pulled the repo - it effing RAN!! Spending 4+ hours on code that someone else can run with no issues makes one want to throw themselves in front of traffic - at times.  After that fiasco, we went through building an app from scratch with a database.  That helped me understand more how the database connects in with the pyramid.  Though I still don't feel like I have any level of competency.  If you ask ... some people.. that is totally fine and expected.  Well, fine.. I guess I'm doing great then.  If our measure of success is how hard we fail... I'm pretty sure I'm at the top of the class.  I'm definitely bucking for 'most hours put in'",
+        "author": "Rob Bronson"
+
+    },
     {
         "title": "Day 14",
         "id": 14,
